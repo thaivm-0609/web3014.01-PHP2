944ecafd36596977ed85021e55abf391474d923e
lovent <vuongthai95@gmail.com>
2024-01-27 13:33:04 +0700

    mvc oop lesson 4

--- mvc_oop/app/controllers/ProductController.php
+++ mvc_oop/app/controllers/ProductController.php
@@ -1,15 +1,40 @@
 <?php
-namespace App\Controllers;
-// require "app/models/Product.php";
+namespace App\Controllers; //quy tắc đặt namespace, viết hoa cữ cái đầu
+
 use App\Models\Product;
 
 class ProductController 
 {
+    public $productModel;
+
+    public function __construct() {
+        $this->productModel = new Product();
+    }
     //trả về danh sách sản phẩm và chỉ định view
     public function listProduct() {
-        $productModel = new Product(); //khởi tạo object từ class Product
-        $products = $productModel->getAllProduct(); //gọi vào hàm getAllProduct
+        $products = $this->productModel->getAllProduct(); //gọi vào hàm getAllProduct
         include "app/views/Products.php"; //chỉ định views sẽ sử dụng
+    }
+
+    public function newPro() {
+        echo "Đây là trang new pro";
+    }
+
+    public function inActivePro() {
+        echo "Đây là trang inactive pro";
+    }
+
+    public function productDetail($id) {
+        
+    }
+
+    public function edit($id) { //trả về form update và fill dữ liệu cũ
+       $product = $this->productModel->getById($id);
+       include "app/views/products/update.php";
+    }
+
+    public function update($id) { //update bản ghi
+
     }
 }
 ?>
